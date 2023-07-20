0f4123c34d12a1d8f79277304f93ee96cec258e0
Omar <you@example.com>
2023-07-20 07:39:13 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
 /**
- * _isupper - check if C is upper
- *
- * @c: input for alphabet
- *
- * Return: 1 if its uppercase, 0 if not
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
 */
-
-
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	else
-		return (0);
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+
 }
